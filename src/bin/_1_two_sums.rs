75d4263512d08a62cba3b1df73bdb6cbb1691b2c
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2025-10-21 11:58:01 -0400

    1. Two sums. Top time solution

--- src/bin/_1_two_sums.rs
+++ src/bin/_1_two_sums.rs
@@ -1,36 +1,34 @@
+use std::collections::HashSet;
 impl Solution {
     fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         if nums.len() < 2 {
             return vec![];
         }
-        let candidate = nums.first().unwrap();
-        let rest = &nums[1..];
-        return Self::find_num(target, candidate, rest, 0);
-    }
-    fn find_num(target: i32, candidate: &i32, rest: &[i32], offset: i32) -> Vec<i32> {
-        if rest.is_empty() {
-            return vec![];
+        let set: HashSet<_> = nums.iter().cloned().collect();
+
+        for (i, &v) in nums.iter().enumerate() {
+            if set.contains(&(target - v)) {
+                let complement = target - v;
+                let rpos = nums.iter().rposition(|&x| x == complement).unwrap();
+                if rpos > i {
+                    return vec![i as i32, rpos as i32];
+                }
+            }
         }
-        let goal = target - candidate;
-        if let Some(index) = rest.iter().position(|&x| x == goal) {
-            return vec![offset, 1 + offset + index as i32];
-        }
-        Self::find_num(target, rest.first().unwrap(), &rest[1..], offset + 1)
+        vec![]
     }
 }
-
-
 
 struct Solution;
 fn main() {}
 
 #[test]
-fn test1(){
+fn test1() {
     let actual = Solution::two_sum(vec![2, 7, 11, 15], 9);
     assert_eq!(actual, vec![0, 1]);
 }
 #[test]
-fn test2(){
+fn test2() {
     let actual = Solution::two_sum(vec![3, 2, 4], 6);
     assert_eq!(actual, vec![1, 2]);
 }
